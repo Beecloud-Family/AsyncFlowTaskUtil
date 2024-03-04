67a128127e733e506bb1ddf322a8fa972fc15ad0
bookiosk <bookiosk@163.com>
2024-03-05 07:21:20 +0800

    修改定时任务注释

--- flowsvr/src/main/java/com/zdf/flowsvr/data/po/AsyncFlowTask.java
+++ flowsvr/src/main/java/com/zdf/flowsvr/data/po/AsyncFlowTask.java
@@ -7,33 +7,63 @@ import lombok.Data;
 public class AsyncFlowTask {
     
     private String id;
-    
-    private String user_id; //NOT NULL DEFAULT '',
-    
-    private String task_id; // NOT NULL DEFAULT '',
-    
-    private String task_type; //NOT NULL DEFAULT '',  存储任务的全类名
-    
-    private String task_stage; //NOT NULL DEFAULT '', 存储任务阶段信息
-    
-    private int status; //tinyint(3) unsigned NOT NULL DEFAULT '0',
-    
-    private int crt_retry_num; //NOT NULL DEFAULT '0' COMMENT '已经重试几次了',
-    
-    private int max_retry_num; //NOT NULL DEFAULT '0' COMMENT '最大能重试几次',
-
+    /**
+     * 创建人id
+     */
+    private String user_id;
+    /**
+     * 自定义任务id类似订单号的含义
+     */
+    private String task_id;
+    /**
+     * 存储任务的全类名
+     */
+    private String task_type;
+    /**
+     * 任务所处阶段,比如这个任务有多个工作,那么目前所干的工作就对应其阶段
+     */
+    private String task_stage;
+    /**
+     * 任务当前执行状态
+     */
+    private int status;
+    /**
+     * 已重试次数
+     */
+    private int crt_retry_num;
+    /**
+     * 最大重试次数
+     */
+    private int max_retry_num;
+    /**
+     * 调度时间:时间越小,调度优先级越高
+     * 创建时, order_time = 当前时间 - priority
+     * 调度之后, order_time = 当前时间 - priority 始终维持优先秒数
+     * 失败重试的话, order_time = 当前时间 + 当前任务的执行时间间隔(时间间隔一般由任务配置来做控制)
+     * 好处就是通过这样的方式和create_time,modify_time语义隔离开来,同时还保证所有任务都有机会执行到哪怕优先级较低
+     * 因为大部分情况用户派发的任务都会默认优先机是1
+     */
     private long order_time;
 
+    /**
+     * 优先级
+     */
     private int priority;
+    /**
+     * 最大重试间隔
+     */
+    private int max_retry_interval;
+    /**
+     * 执行日志:一般用于执行报错日志查看
+     */
+    private String schedule_log;
+    /**
+     * 任务上下文，用户自定义
+     */
+    private String task_context;
     
-    private int max_retry_interval;// int(11) NOT NULL DEFAULT '0' COMMENT '最大重试间隔',
+    private Long create_time;
     
-    private String schedule_log;// varchar(4096) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '调度信息记录',
-    
-    private String task_context;// varchar(8192) COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT '' COMMENT '任务上下文，用户自定义',
-    
-    private Long create_time;// datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    
-    private Long modify_time;// datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    private Long modify_time;
 
 }
